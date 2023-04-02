694fe12c55800494eaeed1d243db4fc33f206cad
ultra <jenya26174611@gmail.com>
2023-04-02 10:40:52 +0300

    update PROJECT

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,21 +1,40 @@
 from flask_wtf import FlaskForm
 from wtforms import (
-StringField,
-PasswordField,
-BooleanField,
-SubmitField,
-validators,
-EmailField
+    StringField,
+    PasswordField,
+    BooleanField,
+    SubmitField,
+    EmailField,
+    validators
 )
 
-class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[validators.DataRequired()])
-    password = PasswordField("Password", validators=[validators.DataRequired()])
-    remmember = BooleanField("Remmember")
-    submit = SubmitField("Login")
 
-class RegisterForm(FlaskForm):
-    username = StringField("Username", validators=[validators.DataRequired()])
-    password = PasswordField("Password", validators=[validators.DataRequired()])
-    email = EmailField("Email", validators=[validators.DataRequired()])
-    submit = SubmitField("Login")
+class LoginForm(FlaskForm):
+    """
+    Login form
+    """
+    username = StringField("Username", validators=[validators.DataRequired(message="Username is required")])
+    password = PasswordField(
+        "Password",
+        validators=[
+            validators.DataRequired(message="Password is required"),
+            validators.Length(min=6, message="Min 6 length of password is required")
+        ]
+    )
+    remember = BooleanField("Remember")
+    submit = SubmitField("Log In")
+
+
+class RegisterForm(LoginForm):
+    """
+    Register form
+    """
+    email = EmailField("Email", validators=[validators.DataRequired(message="Email is required"), validators.Email()])
+    confirm_password = PasswordField(
+        "Confirm Password",
+        validators=[
+            validators.DataRequired(message="Confirm password is required"),
+            validators.EqualTo("password", message="Passwords should match")
+        ]
+    )
+    submit = SubmitField("Register")
