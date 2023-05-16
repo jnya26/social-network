e2de54ff3100d4c91bd8f8213549678f94bcc052
jnya26 <128289816+jnya26@users.noreply.github.com>
2023-05-16 14:45:03 +0300

    Add files via upload
    update

--- migrations/versions/bcb5947d9990_.py
+++ migrations/versions/bcb5947d9990_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'bcb5947d9990'
-down_revision = None
+down_revision = '13312e0d4950'
 branch_labels = None
 depends_on = None
 
